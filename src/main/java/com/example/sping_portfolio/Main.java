a4456e0bc4ec1d66657c7c90a64010f702ee1438
ChiefGuap <69777052+ChiefGuap@users.noreply.github.com>
2021-11-17 09:19:10 -0800

    added some nice notes

--- src/main/java/com/example/sping_portfolio/Main.java
+++ src/main/java/com/example/sping_portfolio/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
-        System.out.println("Done!");
+        System.out.println("This is the GOAT Group");
     }
 
 }
