3f304721f8659483bdf213700f69803d1cac3bd0
dylanroman <dylanroman143@gmail.com>
2021-11-30 09:27:09 -0800

    changed port

--- src/main/java/com/example/sping_portfolio/repositories/ex_db_repo.java
+++ src/main/java/com/example/sping_portfolio/repositories/ex_db_repo.java
@@ -1,4 +1,11 @@
 package com.example.sping_portfolio.repositories;
 
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Transactional
+@Service
 public class ex_db_repo {
+
 }
