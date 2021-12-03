8532dc28cf5cf0df0f10da7931aa67e5ea9be3e6
samkoenig9 <samkoenig100@gmail.com>
2021-12-03 08:12:06 -0800

    stuff

--- src/main/java/com/example/sping_portfolio/services/ex_db_service.java
+++ src/main/java/com/example/sping_portfolio/services/ex_db_service.java
@@ -1,42 +1,42 @@
-package com.example.sping_portfolio.services;
+// package com.example.sping_portfolio.services;
 
-import com.example.sping_portfolio.entities.ex_db_entity;
-import com.example.sping_portfolio.repositories.ex_db_repo;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+// import com.example.sping_portfolio.entities.ex_db_entity;
+// import com.example.sping_portfolio.repositories.ex_db_repo;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
-import java.util.List;
+// import javax.transaction.Transactional;
+// import java.util.List;
 
-//methods for interacting with database
-@Service
-@Transactional
-public class ex_db_service {
-    @Autowired
-    private ex_db_repo jpa;
+// //methods for interacting with database
+// @Service
+// @Transactional
+// public class ex_db_service {
+//     @Autowired
+//     private ex_db_repo jpa;
 
-    //query all
-    public List<ex_db_entity>listALl() {
-        return jpa.findAll();
-    }
+//     //query all
+//     public List<ex_db_entity>listALl() {
+//         return jpa.findAll();
+//     }
 
-    //query based on specific id
-    public ex_db_entity get(long id){
-        return jpa.findById(id).get();
-    }
+//     //query based on specific id
+//     public ex_db_entity get(long id){
+//         return jpa.findById(id).get();
+//     }
 
-    //query based on search, defined in repository
-    //public List<ex_db_entity>listLike(String term) {
-        //return jpa.findBytestStringContainingIgnoreCase(term);
-    //}
+//     //query based on search, defined in repository
+//     //public List<ex_db_entity>listLike(String term) {
+//         //return jpa.findBytestStringContainingIgnoreCase(term);
+//     //}
 
-    //commit to db
-    public void commit(ex_db_entity entry) {
-        jpa.save(entry);
-    }
+//     //commit to db
+//     public void commit(ex_db_entity entry) {
+//         jpa.save(entry);
+//     }
 
-    //delete from db
-    public void delete(long id){
-        jpa.deleteById(id);
-    }
-}
+//     //delete from db
+//     public void delete(long id){
+//         jpa.deleteById(id);
+//     }
+// }
