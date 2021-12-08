3ffb33028097626f9b833b0ec8687128df4a3b02
ChiefGuap <69777052+ChiefGuap@users.noreply.github.com>
2021-12-08 08:59:16 -0800

    Edited my own individual page. Created a controller for my individual page and did some css animations!

--- src/main/java/com/example/sping_portfolio/individual/raquib/controllers/raquib.java
+++ src/main/java/com/example/sping_portfolio/individual/raquib/controllers/raquib.java
@@ -1,5 +1,11 @@
 package com.example.sping_portfolio.individual.raquib.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class raquib {
-    
+    @GetMapping("../individual_pages/raq/raquib.html")
+
+    public String raquib() {
+        return "raquib";
+    }
 }
