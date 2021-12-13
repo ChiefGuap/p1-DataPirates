b706cec055d196e7cb2bb9d1633354889f41974a
ChiefGuap <69777052+ChiefGuap@users.noreply.github.com>
2021-12-13 09:12:40 -0800

    Raq's API

--- src/main/java/com/example/sping_portfolio/API/raq_apicontroller.java
+++ src/main/java/com/example/sping_portfolio/API/raq_apicontroller.java
@@ -1,15 +1,43 @@
 package com.example.sping_portfolio.API;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 
 @Controller
 public class raq_apicontroller {
-    @GetMapping("/raq_api")
 
-    public String raq_api() {
+    // GET request, no parameters
+    @GetMapping("/raq_api")
+    public String News(Model model) throws IOException, InterruptedException, ParseException {
+        //api setup:
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://api-nba-v1.p.rapidapi.com/statistics/players/gameId/5"))
+                .header("x-rapidapi-host", "api-nba-v1.p.rapidapi.com")
+                .header("x-rapidapi-key", "c9cd6ede56msh21fd0a650f12276p14c863jsn63127aad93fa")
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+      //  System.out.println(response.body());
+
+        //alternative #2: convert response.body() to JSON object
+        Object obj = new JSONParser().parse(response.body());
+        JSONObject jo = (JSONObject) obj;
+
+//        System.out.println(jo.get("data"));
+        model.addAttribute("data", jo.get("api" ));
+        System.out.println(jo.get("api"));
+
         return "raq_api";
     }
 }
