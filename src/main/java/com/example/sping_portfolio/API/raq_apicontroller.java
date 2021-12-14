448bc1ebbe8fdc333bfc5e0552d491453938f39a
Billy.C <72890070+BillyCherres@users.noreply.github.com>
2021-12-14 10:09:57 -0800

    raq api

--- src/main/java/com/example/sping_portfolio/API/raq_apicontroller.java
+++ src/main/java/com/example/sping_portfolio/API/raq_apicontroller.java
@@ -48,21 +48,21 @@ public class raq_apicontroller {
     public String News(Model model) throws IOException, InterruptedException, ParseException {
         //api setup:
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://api-nba-v1.p.rapidapi.com/statistics/players/gameId/5"))
-                .header("x-rapidapi-host", "api-nba-v1.p.rapidapi.com")
-                .header("x-rapidapi-key", "c9cd6ede56msh21fd0a650f12276p14c863jsn63127aad93fa")
+                .uri(URI.create("https://imdb8.p.rapidapi.com/auto-complete?q=name"))
+                .header("x-rapidapi-host", "imdb8.p.rapidapi.com")
+                .header("x-rapidapi-key", "7a79b8a230mshff68040a0bd2bf0p1174f8jsnf85b178f3369")
                 .method("GET", HttpRequest.BodyPublishers.noBody())
                 .build();
         HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-      //  System.out.println(response.body());
+//        System.out.println(response.body());
 
         //alternative #2: convert response.body() to JSON object
         Object obj = new JSONParser().parse(response.body());
         JSONObject jo = (JSONObject) obj;
 
 //        System.out.println(jo.get("data"));
-        model.addAttribute("data", jo.get("api" ));
-        System.out.println(jo.get("api"));
+        model.addAttribute("data", jo.get("d" ));
+        System.out.println(jo.get("d"));
 
         return "raq_api";
     }
