b437a5294bf9f32060a2c5789e835f6146c58322
samkoenig9 <samkoenig100@gmail.com>
2021-12-15 09:16:08 -0800

    Revert "Merge branch 'master' of https://github.com/ChiefGuap/p1-DataPirates"
    This reverts commit 0034c736a9eb0e697028ae5b07645daf00c1fbd7, reversing
    changes made to 979398498b8475cbe6535b29640bb4ac5c28e91c.

--- src/main/java/com/example/sping_portfolio/API/sam_apicontroller.java
+++ src/main/java/com/example/sping_portfolio/API/sam_apicontroller.java
@@ -18,26 +18,27 @@ import java.net.http.HttpResponse;
 public class sam_apicontroller {
 
     // GET request, no parameters
-    @GetMapping("/samapi")
+    @GetMapping("/sam_apicontroller")
     public String News(Model model) throws IOException, InterruptedException, ParseException {
         //api setup:
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://mdblist.p.rapidapi.com/?s=jaws"))
-                .header("x-rapidapi-host", "mdblist.p.rapidapi.com")
-                .header("x-rapidapi-key", "7a79b8a230mshff68040a0bd2bf0p1174f8jsnf85b178f3369")
-                .method("GET", HttpRequest.BodyPublishers.noBody())
-                .build();
-        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-//        System.out.println(response.body());
+		.uri(URI.create("https://data-imdb1.p.rapidapi.com/genres/"))
+		.header("x-rapidapi-host", "data-imdb1.p.rapidapi.com")
+		.header("x-rapidapi-key", "32750fe179msh1b6c52dc5ff6064p1aca8djsnc4892ff52ca2")
+		.method("GET", HttpRequest.BodyPublishers.noBody())
+		.build();
+    HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+    // System.out.println(response.body());
+
         //alternative #2: convert response.body() to JSON object
         Object obj = new JSONParser().parse(response.body());
         JSONObject jo = (JSONObject) obj;
 
 //        System.out.println(jo.get("data"));
-        model.addAttribute("data", jo.get("search"));
-        System.out.println(jo.get("search"));
+        model.addAttribute("data", jo.get("d" ));
+        System.out.println(jo.get("d"));
 
-        return "samapi";
+        return "sam_apicontroller";
     }
 }
 
