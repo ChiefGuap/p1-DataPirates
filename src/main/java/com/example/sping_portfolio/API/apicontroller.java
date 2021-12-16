66087f0be8ce737ee78173ac284a0c4c7073e7f3
Billy.C <72890070+BillyCherres@users.noreply.github.com>
2021-12-16 00:27:57 -0800

    frq 2 and 3 completed

--- src/main/java/com/example/sping_portfolio/API/apicontroller.java
+++ src/main/java/com/example/sping_portfolio/API/apicontroller.java
@@ -6,6 +6,8 @@ import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
 import java.net.URI;
@@ -13,6 +15,104 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+class Sequence {
+    String sequence;
+
+    public Sequence(String sequence) {
+        this.sequence = sequence;
+    }
+
+    public String insertSegment(String segment, int ind) {
+        StringBuilder sb = new StringBuilder(segment);
+        sb.insert(ind, segment);
+        return sb.toString();
+    }
+
+    public void changeSequence(String sequence) {
+        this.sequence = sequence;
+    }
+
+    public void display() {
+        System.out.println(sequence);
+    }
+
+}
+
+
+@Controller
+class billy_apicontroller {
+// billy FRQs
+
+// Unit 2
+
+    String week2Frq = "(a) \n" +
+            "LightSequence GradShow = new LightSequence();\n" +
+            "LightSequence(“0101 0101 0101”); \n" +
+            "(b) \n" +
+            "\n" +
+            "gradShow.display(); \n" +
+            " \n" +
+            "(c)\n" +
+            "gradShow.changeSequence(“0011 0011 0011”);\n" +
+            " \n" +
+            "(d) \n" +
+            "String resultSeq = gradShow.insertSegment(\"1111 1111\", 4);\n" +
+            " \n" +
+            "(e) \n" +
+            "int index = oldSeq.indexOf(segment);\n" +
+            "String newSeq = oldSeq.substring(0, index) + oldSeq.substring(index + segment.length());\n" +
+            " \n" +
+            "f)\n" +
+            "double c = Math.sqrt((a * a) + (b * b);\n" +
+            "System.out. println(c); \n" +
+            " \n";
+    @GetMapping("/billyfrq")
+    public String returnBillyFRQ() {
+        return "billyfrq";
+    }
+
+    @GetMapping("/billyfrq_api")
+    @ResponseBody
+    public String[] returnFRQ(@RequestParam(name = "week", required = false, defaultValue = "2") String week) {
+        String[] array = new String[3];
+        array[0] = week2Frq;
+
+        // FRQ Handling
+
+        // Unit 2 FRQ
+        String output = "";
+
+        //A
+        LightSequence GradShow = new LightSequence("0101 0101 0101");
+        //B
+        GradShow.display();
+
+        //C
+        GradShow.changeSequence("0011 0011 0011");
+
+        //D
+
+        String resultSeq = GradShow.insertSegment("1111 1111", 4);
+        output += resultSeq;
+        output += ",";
+        //E
+        String oldSeq = "1100000111";
+        String segment = "11";
+        int index = oldSeq.indexOf(segment);
+        String newSeq = oldSeq.substring(0, index) + oldSeq.substring(index + segment.length());
+        output += newSeq;
+        output += ",";
+        //F
+        int a = 4;
+        int b = 5;
+        double c = Math.sqrt((a * a) + (b * b));
+        output += String.valueOf(c);
+        output += ",";
+
+        array[1] = output;
+        return array;
+    }
+}
 
 @Controller
 public class apicontroller {
