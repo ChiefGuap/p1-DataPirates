f1d4c7ed271f0db27dddd3b8e055e210fcf8d363
ChiefGuap <69777052+ChiefGuap@users.noreply.github.com>
2022-01-06 23:15:23 -0800

    Revert "test"
    This reverts commit 6ca00a89ede6c25681a943bcb19bdc55d36ba334.

--- src/main/java/com/example/sping_portfolio/controllers/anime_rating_controller.java
+++ src/main/java/com/example/sping_portfolio/controllers/anime_rating_controller.java
@@ -18,7 +18,7 @@ public class anime_rating_controller {
 
     //Edit Rating List
     @GetMapping("/ar2022/admin")
-    public String ar2022_admin(Model model) {
+    public String schedule(Model model) {
         //query database
         List<anime_rating> ar_query = ar.listALl();
 
@@ -27,7 +27,7 @@ public class anime_rating_controller {
     }
 
     @PostMapping("/ar2022/admin")
-    public String ar2022_admin(@RequestParam("rank") int rank,
+    public String schedule(@RequestParam("rank") int rank,
                            @RequestParam("name") String name,
                            @RequestParam("rating") int rating,
                            @RequestParam("review") String review) {
@@ -37,45 +37,5 @@ public class anime_rating_controller {
         ar.commit(entry);
 
         return "redirect:/ar2022/admin";
-    }
-
-    @GetMapping("/ar2022/admin/delete")
-    public String ar2022_admin_delete(Model model) {
-        //query database
-        List<anime_rating> ar_query = ar.listALl();
-
-        model.addAttribute("ar_query", ar_query);
-        return "anime_rating_delete";
-    }
-
-    @PostMapping("/ar2022/admin/delete")
-    public String ar2022_admin_delete(@RequestParam("id") long id) {
-        //delete entry
-        ar.delete(id);
-
-        return "redirect:/ar2022/admin/delete";
-    }
-
-    @GetMapping("/ar2022/admin/edit")
-    public String ar2022_admin_edit(Model model) {
-        //query database
-        List<anime_rating> ar_query = ar.listALl();
-
-        model.addAttribute("ar_query", ar_query);
-        return "anime_rating_edit";
-    }
-
-    @PostMapping("/ar2022/admin/edit")
-    public String ar2022_admin_edit(@RequestParam("id") long id,
-                                    @RequestParam("rank") int rank,
-                                    @RequestParam("name") String name,
-                                    @RequestParam("rating") int rating,
-                                    @RequestParam("review") String review) {
-        //edit entry
-        anime_rating entry = new anime_rating(rank, name, rating, review);
-        entry.setId(id);
-        ar.commit(entry);
-
-        return "redirect:/ar2022/admin/edit";
     }
 }
