4699e3a7540d45ddbc7cfd31f70f8ce2b425c8dd
ChiefGuap <69777052+ChiefGuap@users.noreply.github.com>
2022-02-11 08:28:46 -0800

    Working on main page Movie API Tutorial

---
+++ src/main/resources/static/styles/movies.js
@@ -0,0 +1,23 @@
+fetch("https://imdb8.p.rapidapi.com/auto-complete?q=game%20of%20th", {
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-host": "imdb8.p.rapidapi.com",
+        "x-rapidapi-key": "ae22dfb14emshbb373a2396ae162p17004djsnc49b06d9ca8b"
+    }
+})
+    .then(response => response.json())
+    .then(data => {
+        const list = data.d;
+
+    })
+
+    list.map(item) => {
+    const name = item.l;
+    const poster = item.i.imageUrl;
+    const movie = `<li><img src="${poster}"><h2>${name}</h2> </li>
+    document.querySelector(".movies").innerHTML += movies; 
+    
+})
+    .catch(err => {
+        console.error(err);
+    });
