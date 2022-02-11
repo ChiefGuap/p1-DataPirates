729346cc86d000f9a47fb49f41932101af728d66
samkoenig9 <samkoenig100@gmail.com>
2022-02-11 09:00:00 -0800

    added dane stuff

---
+++ src/main/java/com/example/sping_portfolio/individual/dane/controllers/dane.java
@@ -0,0 +1,19 @@
+package com.example.sping_portfolio.individual.dane.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+public class dane {
+
+    @GetMapping("/dane")
+    public String sam() {
+        return "dane";
+    }
+}
