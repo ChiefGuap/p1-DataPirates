7e7b3025545394c3648b458f6d7608f42fc37fab
ChiefGuap <69777052+ChiefGuap@users.noreply.github.com>
2022-02-27 23:40:04 -0800

    25th time is the charm. finished working on api, finished watching the tutorial!

--- src/main/resources/static/styles/movies.js
+++ src/main/resources/static/styles/movies.js
@@ -1,4 +1,7 @@
-const API_KEY = 'api_key=b2662475a42046ca0e5f0f4f3b3b1c41';
+//TMDB
+//TMDB
+
+const API_KEY = 'api_key=1cf50e6248dc270629e802686245c2c8';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
@@ -210,11 +213,14 @@ function showMovies(data) {
         movieEl.classList.add('movie');
         movieEl.innerHTML = `
              <img src="${poster_path? IMG_URL+poster_path: "http://via.placeholder.com/1080x1580" }" alt="${title}">
+
             <div class="movie-info">
                 <h3>${title}</h3>
                 <span class="${getColor(vote_average)}">${vote_average}</span>
             </div>
+
             <div class="overview">
+
                 <h3>Overview</h3>
                 ${overview}
                 <br/> 
@@ -265,6 +271,7 @@ function openNav(movie) {
         
         ${embed.join('')}
         <br/>
+
         <div class="dots">${dots.join('')}</div>
         
         `
